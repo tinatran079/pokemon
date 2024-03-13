40ba11f6237078670bc061a7d9001e21561e6597
Tina Tran <tinatran079@gmail.com>
2024-03-13 14:48:30 -0500

    list of pokemon

--- src/App.js
+++ src/App.js
@@ -1,9 +1,40 @@
 import React, { useState, useEffect } from "react";
 import PokemonList from "./PokemonList";
+import axios from "axios";
 
 function App() {
   // list of pokemon
-  const [pokemon, setPokemon] = useState(["bulbasaur", "charmander"]);
+  const [pokemon, setPokemon] = useState([]);
+
+  // track current page
+  const [currentPageUrl, setCurrentPageUrl] = useState(
+    "https://pokeapi.co/api/v2/pokemon"
+  );
+
+  const [nextPageUrl, setNextPageUrl] = useState();
+  const [prevPageUrl, setPrevPageUrl] = useState();
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setLoading(true);
+    let cancel;
+    axios
+      .get(currentPageUrl, {
+        cancelToken: new axios.CancelToken((c) => (cancel = c)),
+      })
+      .then((res) => {
+        setLoading(false);
+        setNextPageUrl(res.data.next);
+        setPrevPageUrl(res.data.previous);
+        setPokemon(res.data.results.map((p) => p.name));
+      });
+
+    // gets called everytime useEffect gets called again
+    // cancel prev request when making new req
+    return () => cancel();
+  }, [currentPageUrl]);
+
+  if (loading) return "Loading...";
 
   return <PokemonList pokemon={pokemon} />;
 }
